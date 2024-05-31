6bc787bff6040fe4335a8db55a1365674d0eee0f
Noah Lisin <noahl25@lakesideschool.org>
2024-05-30 22:43:49 -0700

    fixed

--- Assets/PlayerScripts/TestRoomManager.cs
+++ Assets/PlayerScripts/TestRoomManager.cs
@@ -25,7 +25,7 @@ public class TestRoomManager : MonoBehaviourPunCallbacks
     private RoomList.GameType gameType;
 
     private string map = "MainMap";
-    private string[] mapList = {"MainMap", "Map2", "Map3"} ;
+    private string[] mapList = {"MainMap", "Map3"};
 
 
     // Start is called before the first frame update
@@ -36,6 +36,18 @@ public class TestRoomManager : MonoBehaviourPunCallbacks
         Cursor.lockState = CursorLockMode.None;
 
         //DontDestroyOnLoad(this.gameObject);
+
+        if (PhotonNetwork.InRoom) {
+            spawnPoint = LobbySpawnPoint.Position(PhotonNetwork.CurrentRoom.PlayerCount);
+            //_player = PhotonNetwork.Instantiate(player.name, spawnPoint, Quaternion.identity);
+            _player = PhotonNetwork.Instantiate(player.name, spawnPoint, Quaternion.Euler(0,180,0));
+
+            _player.GetComponent<PhotonView>().RPC("SetName", RpcTarget.OthersBuffered, playerName, playerTeam);
+            _player.GetComponent<PlayerSetup>().HideName();
+            _player.GetComponent<PhotonView>().RPC("SetupMeshes", RpcTarget.OthersBuffered);
+            _player.GetComponent<PlayerSetup>().LobbySetup();
+            return;
+        }
 
         readyButtonText = FindObjectOfType<TextMeshProUGUI>();
         Debug.Log("TestJoinLobby");
@@ -56,6 +68,7 @@ public class TestRoomManager : MonoBehaviourPunCallbacks
         roomOptions.CustomRoomProperties = properties;
 
         //Creating room
+
         PhotonNetwork.JoinOrCreateRoom(roomNameToJoin, roomOptions, null);
         
 
@@ -98,14 +111,8 @@ public class TestRoomManager : MonoBehaviourPunCallbacks
     }
     public void Leave()
     {        
-        PhotonView photonView = PhotonView.Get(this);
-        playerReadyStatus = false;
-        photonView.RPC("ReadyRPC", RpcTarget.All, playerReadyStatus);
-        GameObject gameSelect = GameObject.FindWithTag("GameSelection");
-        Destroy(gameSelect);
         PhotonNetwork.LeaveRoom();
         PhotonNetwork.Disconnect();
-
         //yield return new WaitUntil(() => !PhotonNetwork.IsConnected);
         //PhotonNetwork.ConnectUsingSettings();
         Debug.Log("Left Room");
